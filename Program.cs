6441061ed3d506aea372c23c855af94e916734ee
Mateusz Cwilag <mateuszcwilag@outlook.com>
2020-03-29 12:41:52 +0200

    master changes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
